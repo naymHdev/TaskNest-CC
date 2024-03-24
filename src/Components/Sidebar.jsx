f329088079e621019b585a215b197079a521689e
MacBook Pro <naym100m@gmail.com>
2024-03-24 13:08:01 +0600

    sidebar totaly updated and modified in task sidebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,62 +1,60 @@
 import {
-    SquaresPlusIcon,
-    Cog6ToothIcon,
-    ChatBubbleOvalLeftEllipsisIcon,
-    UserCircleIcon,
-  } from '@heroicons/react/24/solid';
+  MdDashboardCustomize,
+  MdSettings,
+  MdOutlineHome,
+} from "react-icons/md";
+import { CgProfile } from "react-icons/cg";
 
+import { NavLink } from "react-router-dom";
 
-  import { NavLink } from 'react-router-dom';
-  
-  const Sidebar = () => {
-    return (
-      <div className="h-screen sticky top-0 border-r-2 border-secondary/20">
-        <div className="flex flex-col items-center gap-5 h-full py-5">
-          {/* <img src={logo} alt="logo" /> */}
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              isActive
-                ? 'p-2 rounded-2xl bg-primary text-white cursor-pointer'
-                : 'p-2 rounded-2xl group hover:bg-primary text-secondary/40 cursor-pointer transition-all'
-            }
-          >
-            <SquaresPlusIcon className="h-7 w-7 group-hover:text-white" />
-          </NavLink>
-          <NavLink
-            to="/chat"
-            className={({ isActive }) =>
-              isActive
-                ? 'p-2 rounded-2xl bg-primary text-white cursor-pointer'
-                : 'p-2 rounded-2xl group hover:bg-primary text-secondary/40 cursor-pointer transition-all'
-            }
-          >
-            <ChatBubbleOvalLeftEllipsisIcon className="h-7 w-7 group-hover:text-white " />
-          </NavLink>
-          <NavLink
-            to="/settings"
-            className={({ isActive }) =>
-              isActive
-                ? 'p-2 rounded-2xl bg-primary text-white cursor-pointer'
-                : 'p-2 rounded-2xl group hover:bg-primary text-secondary/40 cursor-pointer transition-all'
-            }
-          >
-            <Cog6ToothIcon className="h-7 w-7 group-hover:text-white " />
-          </NavLink>
-          <NavLink
-            to="/profile"
-            className={({ isActive }) =>
-              isActive
-                ? 'p-2 rounded-2xl bg-primary text-white cursor-pointer mt-auto'
-                : 'p-2 rounded-2xl group hover:bg-primary text-secondary/40 cursor-pointer transition-all  mt-auto'
-            }
-          >
-            <UserCircleIcon className="h-7 w-7 group-hover:text-white " />
-          </NavLink>
-        </div>
+const Sidebar = () => {
+  return (
+    <div className="h-screen sticky top-0 border-r-2 border-[#D68030]">
+      <div className="flex flex-col items-center gap-5 h-full py-5">
+        {/* <img src={logo} alt="logo" /> */}
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive
+              ? "p-2 rounded-2xl bg-slate-700 text-[#D68030] cursor-pointer"
+              : "p-2 rounded-2xl group hover:bg-slate-700 text-secondary/40 cursor-pointer transition-all"
+          }
+        >
+          <MdOutlineHome className="h-7 w-7 text-[#D68030]" />
+        </NavLink>
+        <NavLink
+          to="/dashboard"
+          className={({ isActive }) =>
+            isActive
+              ? "p-2 rounded-2xl bg-slate-700 text-[#D68030] cursor-pointer"
+              : "p-2 rounded-2xl group hover:bg-slate-700 text-secondary/40 cursor-pointer transition-all"
+          }
+        >
+          <MdDashboardCustomize className="h-7 w-7 text-[#D68030]" />
+        </NavLink>
+        <NavLink
+          to="/settings"
+          className={({ isActive }) =>
+            isActive
+              ? "p-2 rounded-2xl bg-slate-700 text-white cursor-pointer"
+              : "p-2 rounded-2xl group hover:bg-slate-700 text-secondary/40 cursor-pointer transition-all"
+          }
+        >
+          <MdSettings className="h-7 w-7 text-[#D68030] " />
+        </NavLink>
+        <NavLink
+          to="/profile"
+          className={({ isActive }) =>
+            isActive
+              ? "p-2 rounded-2xl bg-slate-700 text-white cursor-pointer mt-auto"
+              : "p-2 rounded-2xl group hover:bg-slate-700 text-secondary/40 cursor-pointer transition-all  mt-auto"
+          }
+        >
+          <CgProfile className="h-7 w-7 text-[#D68030] " />
+        </NavLink>
       </div>
-    );
-  };
-  
-  export default Sidebar;
-  
+    </div>
+  );
+};
+
+export default Sidebar;
