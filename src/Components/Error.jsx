0965c9c5664efaee7686dfd869748d24e88fc7bc
MacBook Pro <naym100m@gmail.com>
2024-03-24 11:41:34 +0600

    all project folder setup compleate

---
+++ src/Components/Error.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+
+const Error = () => {
+    return (
+        <div>
+            <Link to="/"> <button className="btn px-2 py-4 rounded-md">Back to Home</button></Link>
+        </div>
+    );
+};
+
+export default Error;
