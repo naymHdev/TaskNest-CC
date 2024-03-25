bbfee23d6cc99f9633114067037e0398409563c1
MacBook Pro <naym100m@gmail.com>
2024-03-25 20:35:23 +0600

    update a add task form

--- src/Dashboard/AddTask.jsx
+++ src/Dashboard/AddTask.jsx
@@ -1,9 +1,13 @@
 import { Modal } from "antd";
 import { useState } from "react";
 import { CgPlayListAdd } from "react-icons/cg";
+import AddTaskForm from "./AddTaskForm";
 
 const AddTask = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [tasks, setTasks] = useState([]);
+
+  // // Modal Handel
   const showModal = () => {
     setIsModalOpen(true);
   };
@@ -13,6 +17,13 @@ const AddTask = () => {
   const handleCancel = () => {
     setIsModalOpen(false);
   };
+
+  // Task Handel
+  const handleSubmit = (data) => {
+    setTasks([...tasks, data]);
+  };
+
+  console.log("tasks are -----------",tasks);
 
   return (
     <>
@@ -31,8 +42,16 @@ const AddTask = () => {
       >
         <section>
           <div>
-            <h3 className=" text-2xl font-medium text-slate-800">Create a New Task</h3>
-            <p className=" text-slate-600">Add your teammate to your team and start working together on going done!</p>
+            <h3 className=" text-2xl font-medium text-slate-800">
+              Create a New Task
+            </h3>
+            <p className=" text-slate-600 mt-2">
+              Add your teammate to your team and start working together on going
+              done!
+            </p>
+          </div>
+          <div>
+            <AddTaskForm onSubmit={handleSubmit} />
           </div>
         </section>
       </Modal>
---
+++ src/Dashboard/AddTaskForm.jsx
@@ -0,0 +1,127 @@
+/* eslint-disable react/prop-types */
+import { useForm } from "react-hook-form";
+
+const AddTaskForm = ({ onSubmit }) => {
+  const {
+    reset,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmitHandler = (data) => {
+    onSubmit(data);
+  };
+
+  const handleTaskCancel = () => {
+    reset();
+  };
+
+  return (
+    <div className=" w-full">
+      <form onSubmit={handleSubmit(onSubmitHandler)} className="px-3 pt-6 mb-4">
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="title"
+          >
+            Title
+          </label>
+          <input
+            {...register("title", { required: true })}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="title"
+            type="text"
+            placeholder="Title"
+          />
+          {errors.title && (
+            <span className="text-red-500 text-xs">Title is required</span>
+          )}
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="description"
+          >
+            Description
+          </label>
+          <textarea
+            {...register("description", { required: true })}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="description"
+            placeholder="Description"
+          ></textarea>
+          {errors.description && (
+            <span className="text-red-500 text-xs">
+              Description is required
+            </span>
+          )}
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="team"
+          >
+            Team
+          </label>
+          <input
+            {...register("team")}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="team"
+            type="text"
+            placeholder="Team"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="assignee"
+          >
+            Assignee
+          </label>
+          <input
+            {...register("assignee")}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="assignee"
+            type="text"
+            placeholder="Assignee"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="priority"
+          >
+            Priority
+          </label>
+          <select
+            {...register("priority")}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="priority"
+          >
+            <option value="low">Low</option>
+            <option value="medium">Medium</option>
+            <option value="high">High</option>
+          </select>
+        </div>
+        <div className="flex items-center justify-end gap-3">
+          <button
+            onClick={handleTaskCancel}
+            className="bg-gray-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+          >
+            Cancel
+          </button>
+          <button
+            className="bg-[#FA963A] hover:bg-orange-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AddTaskForm;
