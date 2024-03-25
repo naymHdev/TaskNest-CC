a48c1427acf4777793ff4564f42b75223eb34a59
MacBook Pro <naym100m@gmail.com>
2024-03-25 23:12:53 +0600

    task card fully compleated in the task section

--- src/Pages/MyTask/MyTask.jsx
+++ src/Pages/MyTask/MyTask.jsx
@@ -3,10 +3,42 @@ import TaskCard from "./TaskCard";
 const MyTask = () => {
   return (
     <>
-      <section className=" grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 bg-[#F9F9F9] p-2 rounded-md">
+      <section className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4 bg-[#F9F9F9] p-2 rounded-md">
         <div>
           <div className=" rounded-md shadow-md bg-[#EEEEEE] py-3">
             <h3 className=" text-xl font-medium text-center">Pending</h3>
+          </div>
+          <div>
+            <TaskCard />
+          </div>
+        </div>
+        <div>
+          <div className=" rounded-md shadow-md bg-[#F8EDC8] py-3">
+            <h3 className=" text-xl font-medium text-center">In Progress</h3>
+          </div>
+          <div>
+            <TaskCard />
+          </div>
+        </div>
+        <div>
+          <div className=" rounded-md shadow-md bg-[#C8F8CD] py-3">
+            <h3 className=" text-xl font-medium text-center">Completed</h3>
+          </div>
+          <div>
+            <TaskCard />
+          </div>
+        </div>
+        <div>
+          <div className=" rounded-md shadow-md bg-[#c3c7fb] py-3">
+            <h3 className=" text-xl font-medium text-center">Deployed</h3>
+          </div>
+          <div>
+            <TaskCard />
+          </div>
+        </div>
+        <div>
+          <div className=" rounded-md shadow-md bg-[#C8EBF8] py-3">
+            <h3 className=" text-xl font-medium text-center">Deferred</h3>
           </div>
           <div>
             <TaskCard />
--- src/Pages/MyTask/TaskCard.jsx
+++ src/Pages/MyTask/TaskCard.jsx
@@ -1,20 +1,43 @@
-import { LuArrowUpRightFromCircle } from "react-icons/lu";
+import { PiDotsThreeCircleVerticalDuotone } from "react-icons/pi";
 
 const TaskCard = () => {
   return (
     <>
       <section className={`mt-5 bg-[#EEEEEE] rounded-xl p-2 shadow-md`}>
-        <div className=" flex items-center justify-between px-2 font-semibold">
-          <p>18 Feb 2024</p>{" "}
-          <LuArrowUpRightFromCircle className=" text-xl hover:cursor-pointer" />
+        <div className=" flex items-center justify-between font-semibold">
+          <p>18 Feb 2024</p>
+          <div className="dropdown dropdown-hover">
+            <div tabIndex={0}>
+              <PiDotsThreeCircleVerticalDuotone className=" text-2xl hover:cursor-pointer" />
+            </div>
+            <ul
+              tabIndex={0}
+              className="dropdown-content z-[1] menu bg-[#F9F9F9] rounded-md w-28"
+            >
+              <p className=" font-bold hover:cursor-pointer p-2 hover:text-green-500">
+                Edit
+              </p>
+              <hr className="mt-[1px] mb-[1px]" />
+              <p className=" font-bold hover:cursor-pointer p-2 hover:text-red-500">
+                Delete
+              </p>
+            </ul>
+          </div>
         </div>
-        <h3 className=" text-xl font-medium mt-5">
-          Testing and user feedback for dribble short{" "}
-        </h3>
-        <div className=" flex items-center justify-between px-1 mt-5">
-          <p className=" text-sm font-medium">09.00AM - 10.00PM</p>
-          <div className="avatar">
-            <div className="w-8 rounded-full">
+        <div className="mt-3 space-y-3">
+          <h3 className=" text-xl font-medium">
+            Testing and user feedback for dribble short{" "}
+          </h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil,
+            voluptatum!
+          </p>
+        </div>
+        <div className=" flex items-center justify-between mt-5">
+          <p className=" font-semibold">Priority</p>
+          {/* TODO: Add tooltip name daynamicaly */}
+          <div className="avatar tooltip" data-tip="Name">
+            <div className="w-10 rounded-full">
               <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
             </div>
           </div>
