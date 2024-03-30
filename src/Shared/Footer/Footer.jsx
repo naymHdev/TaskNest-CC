4e9bd55a5fb9554e0af1eb57b8d076e6b3037765
MacBook Pro <naym100m@gmail.com>
2024-03-30 17:51:04 +0600

    update and modified footer section

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,92 +1,48 @@
-import { Link } from "react-router-dom";
-import { FaFacebookSquare, FaGithubSquare, FaLinkedinIn } from "react-icons/fa";
-import { RxResume } from "react-icons/rx";
-
 /* eslint-disable react/no-unescaped-entities */
+
+import { FaFacebookSquare, FaGithubSquare, FaLinkedinIn } from "react-icons/fa";
+
 const Footer = () => {
   return (
-    <div>
-      <footer className="footer mt-16 p-2 md:mt-0 md:p-10 bg-base-200 text-base-content">
-        <aside>
-          <h2 className="text-4xl font-bold text-gray-800">
-            <span className=" text-orange-400">Task</span>mate
-          </h2>
-          <p>
-            Join millions of people who organize <br /> work and life with
-            Todoist.
-          </p>
-        </aside>
-        <footer className="grid md:grid-cols-4 grid-cols-2">
-          <nav className="font-medium menu">
-            <header className="footer-title text-xl">Features</header>
-            <a className="link link-hover">How it's Works</a>
-            <Link to="forTeam">
-              <a className="link link-hover">For Team</a>
-            </Link>
-            <Link to="pricing">
-              <a className="link link-hover">Pricing</a>
-            </Link>
-            <Link to="/">
-              <a className="link link-hover">Templates</a>
-            </Link>
-          </nav>
-          <nav className="font-medium menu">
-            <header className="footer-title text-xl">Resources</header>
-            <a className="link link-hover">Download Apps</a>
-            <a className="link link-hover">Help Center</a>
-            <a className="link link-hover">Productivity Methods</a>
-            <a className="link link-hover">Integrations</a>
-            <a className="link link-hover">Developer API</a>
-            <a className="link link-hover">Status</a>
-          </nav>
-          <nav className="font-medium menu">
-            <header className="footer-title text-xl">Company</header>
-            <a className="link link-hover">About Us</a>
-            <a className="link link-hover">Careers</a>
-            <a className="link link-hover">Inspiration Hub</a>
-            <a className="link link-hover">Press</a>
-            <a className="link link-hover">Twist</a>
-          </nav>
-          <nav>
-            <header className="footer-title text-xl">Social</header>
-            <div className=" flex gap-3 items-center text-3xl">
-              <a
-                className=" text-[#0C63BC]"
-                href="https://www.linkedin.com/in/naymhossen1b"
-              >
-                <FaLinkedinIn />
-              </a>
-              <a
-                className=" text-[#1d1a1a]"
-                href="https://github.com/naymhossen1b"
-              >
-                <FaGithubSquare />
-              </a>
-              <a
-                className=" text-[#3b5998]"
-                href="https://www.facebook.com/naymhossen1b"
-              >
-                <FaFacebookSquare />
-              </a>
-              <a
-                className=" text-[#255565]"
-                href="https://naymhossen1b.netlify.app/"
-              >
-                <RxResume />
-              </a>
-            </div>
-            <div className="hidden md:join mt-10 -ml-16">
-              <input
-                type="text"
-                placeholder="username@site.com"
-                className="input input-bordered join-item"
-              />
-              <button className="btn btn-primary join-item">Subscribe</button>
-            </div>
-          </nav>
-        </footer>
-      </footer>
-    </div>
+    <footer className="footer p-10 bg-slate-700 text-neutral-content">
+      <aside>
+       <div className=" flex items-center">
+       <svg
+          width="50"
+          height="50"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          fillRule="evenodd"
+          clipRule="evenodd"
+          className="fill-current"
+        >
+          <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
+        </svg>
+        <h1 className="text-5xl text-[#F89A67] font-bold">
+            Task<span className=" text-slate-50">mate</span>
+          </h1>
+       </div>
+        <p>
+          Taskmate Industries Ltd.
+          <br />
+          Providing reliable tech since 2023
+        </p>
+      </aside>
+      <nav>
+        <h6 className="footer-title text-xl font-bold text-white">Social</h6>
+        <div className="grid grid-flow-col gap-4 text-2xl">
+          <a target="_blank" href="https://web.facebook.com/naymHdev">
+            <FaFacebookSquare className=" text-sky-600" />
+          </a>
+          <a target="_blank" href="https://www.linkedin.com/in/naymhdev">
+            <FaLinkedinIn className=" text-sky-600" />
+          </a>
+          <a target="_blank" href="">
+            <FaGithubSquare className="https://github.com/naymHdev" />
+          </a>
+        </div>
+      </nav>
+    </footer>
   );
 };
 
